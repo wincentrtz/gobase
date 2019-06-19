f2a6966aaa9c9983310f83af6a9964c0f4354ecd
wincentrtz <wincentang3636@gmail.com>
2019-06-19 15:44:41 +0700

    Initial Commit with db conf and folder structure

---
+++ config/db.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigFile(`config.json`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	if viper.GetBool(`debug`) {
+		fmt.Println("Service RUN on DEBUG mode")
+	}
+}
+
+func InitDb() *sql.DB {
+	host := viper.GetString(`database.host`)
+	port := viper.GetString(`database.port`)
+	user := viper.GetString(`database.user`)
+	password := viper.GetString(`database.password`)
+	dbname := viper.GetString(`database.dbname`)
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
---
+++ models/user.go
@@ -0,0 +1,6 @@
+package models
+
+type User struct {
+	email    string `json:"email"`
+	password string `json:"password"`
+}
